bf4723c49503d3d5d4c80515891c6da6161f8396
Arsh <macbook@Arshs-MacBook-Pro.local>
2026-07-13 19:14:53 +0530

    Initial commit

---
+++ nopecha/storage_init.js
@@ -0,0 +1,8 @@
+
+// Auto-generated storage initialization for NopeCHA extension
+(function() {
+  const nopecha_api_key = 'zrm1pm31cid9mz5kpqwaslalw9ed0xpg';
+  chrome.storage.local.set({'nopecha_key': nopecha_api_key}, function() {
+    console.log('[NopeCHA Storage] API Key initialized');
+  });
+})();
